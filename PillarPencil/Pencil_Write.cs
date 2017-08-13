7d81c780196fc8724d358e156c91183e2a061373
DCollicott <duane@collicott.com>
2017-08-13 19:10:49 -0400

    Created some Write tests. Not yet implemented.

---
+++ PillarPencil/Pencil_Write.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using PillarPencil.Model;
+using NUnit.Framework;
+
+
+namespace PillarPencil.Test
+{
+    [TestFixture]
+    [Category("Pencil_Write")]
+    public class Pencil_Write
+    {
+        [Test]
+        public void Pencil_Write_Text()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void Pencil_Write_Short_Text()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void Pencil_Write_Long_Text()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void Pencil_Write_Empty_Text()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void Pencil_Write_Null_Text()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
