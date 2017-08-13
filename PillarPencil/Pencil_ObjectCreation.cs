20786b8c1e066bdf14490978ce2ad7c324c881a3
DCollicott <duane@collicott.com>
2017-08-13 18:49:40 -0400

    Test project create. First test created.

---
+++ PillarPencil/Pencil_ObjectCreation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace PillarPencil.Test
+{
+    [TestFixture]
+    [Category("Pencil_Object")]
+    public class Pencil_ObjectCreation
+    {
+        [Test]
+        public void Pencil_Create_Object()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
