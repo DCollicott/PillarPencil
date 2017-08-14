b2ea2a7cd1948e2e4fab2c39081a4675b9a19c7e
DCollicott <duane@collicott.com>
2017-08-13 21:26:07 -0400

    Sharpening tests, not yet implemented.

---
+++ PillarPencil/Pencil_Sharpen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using PillarPencil.Model;
+using NUnit.Framework;
+
+
+namespace PillarPencil.Test
+{
+    [TestFixture]
+    [Category("Pencil_Sharpen")]
+    public class Pencil_Sharpen
+    {
+        [Test]
+        public void Pencil_Sharpen_Resets_Durability()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void Pencil_Sharpen_Decreases_Length()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void Pencil_Sharpen_Zero_Length_No_Effect()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void Pencil_Sharpen_Full_Durability_No_Effect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
