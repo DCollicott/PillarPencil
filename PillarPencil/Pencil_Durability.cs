8857683118b89537f3c15b34eab17577d82e6e7f
DCollicott <duane@collicott.com>
2017-08-13 20:19:34 -0400

    Durability tests created. Not yet implemented.

---
+++ PillarPencil/Pencil_Durability.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using PillarPencil.Model;
+using NUnit.Framework;
+
+
+namespace PillarPencil.Test
+{
+    [TestFixture]
+    [Category("Pencil_Durability")]
+    public class Pencil_Durability
+    {
+        [Test]
+        public void Pencil_Write_LowerCase_Reduces_Durability()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void Pencil_Write_UpperCase_Reduces_Durability()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void Pencil_Write_WhiteSpace_Doesnt_Reduce_Durability()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void Pencil_Write_Partial_With_Insufficient_Durability()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void Pencil_Cant_Write_With_Zero_Durability()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        [Test]
+        public void Pencil_Sharpen_Resets_Durability()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
